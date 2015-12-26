b772670b59217ef97fbb2086f03580841d667462
BKHMSI <badr@khamissi.com>
2015-12-26 22:09:26 +0200

    User Class added Explore view controller added Map View added to tabbed view controller

---
+++ CarPooling/User.swift
@@ -0,0 +1,73 @@
+//
+//  User .swift
+//  CarPooling
+//
+//  Created by Badr AlKhamissi on 12/26/15.
+//  Copyright © 2015 Badr AlKhamissi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class User {
+    var aucId:String
+    var userName:String
+    var password:String
+    var mobile:NSNumber
+    var photo:UIImage?
+    var points:Int?
+    
+    init(aucId:String, userName:String, password:String, mobile:NSNumber){
+        self.aucId = aucId
+        self.userName = userName
+        self.password = password
+        self.mobile = mobile
+    }
+    
+    // MARK: Getters
+    
+    func getId()->String{
+        return aucId
+    }
+    
+    func getUserName()->String{
+        return userName;
+    }
+    
+    func getPassword()->String{
+        return password;
+    }
+    
+    func getMobile()->NSNumber{
+        return mobile;
+    }
+    
+    func getPoints()->Int{
+        if (points != nil){
+            return points!
+        }else {
+            return 0;
+        }
+    }
+    
+    // MARK: Setters
+    
+    func setId(aucId:String){
+        self.aucId = aucId
+    }
+    
+    func setUserName(userName:String){
+        self.userName = userName
+    }
+    
+    func setPassword(password:String){
+        self.password = password
+    }
+    
+    func setPhoto(photo:UIImage){
+        self.photo = photo
+    }
+
+
+
+}
