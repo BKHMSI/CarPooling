fff78e3d797ce330ddaadae39dc7a65748e53c24
BKHMSI <badr@khamissi.com>
2015-12-27 13:05:59 +0200

    SignUp View Controller with its corresponding outlets and actions Profile Tab added

---
+++ CarPooling/SignUpVC.swift
@@ -0,0 +1,87 @@
+//
+//  SignUpVC.swift
+//  CarPooling
+//
+//  Created by Badr AlKhamissi on 12/27/15.
+//  Copyright © 2015 Badr AlKhamissi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class ProfileVC: UIViewController {
+    
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var aucIdTxtFld: UITextField!
+    @IBOutlet weak var mobileTxtFld: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.hidden = true
+        activityIndicator.hidesWhenStopped = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func signUpBtnPressed(sender: AnyObject) {
+        // Build the terms and conditions alert
+        let alertController = UIAlertController(title: "Agree to terms and conditions",
+            message: "Click I AGREE to signal that you agree to the End User Licence Agreement.",
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alertController.addAction(UIAlertAction(title: "I AGREE",
+            style: UIAlertActionStyle.Default,
+            handler: { alertController in self.processSignUp()})
+        )
+        alertController.addAction(UIAlertAction(title: "I do NOT agree",
+            style: UIAlertActionStyle.Default,
+            handler: nil)
+        )
+        
+        // Display alert
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func processSignUp() {
+        
+        var userEmailAddress = emailTxtFld.text
+        let userPassword = passwordTxtFld.text
+        
+        // Ensure username is lowercase
+        userEmailAddress = userEmailAddress!.lowercaseString
+        
+        // Add email address validation
+        
+        // Start activity indicator
+        activityIndicator.hidden = false
+        activityIndicator.startAnimating()
+        
+        // Create the user
+        let user = PFUser()
+        user.username = userEmailAddress
+        user.password = userPassword
+        user.email = userEmailAddress
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if error == nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("signInToNavigation", sender: self)
+                }
+            } else {
+                self.activityIndicator.stopAnimating()
+                if let message: AnyObject = error!.userInfo["error"] {
+                    //self.message.text = "\(message)"
+                    print("Error: \(message)")
+                }				
+            }
+        }
+    }
+  
+}
