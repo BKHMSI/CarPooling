89971c0e95dededb504d42f58e4fd92d9225803e
BKHMSI <badr@khamissi.com>
2016-01-18 16:36:24 +0200

    Many Commits that I forgot

--- CarPooling/ProfileVC.swift
+++ CarPooling/ProfileVC.swift
@@ -16,6 +16,7 @@ class DataCell:UITableViewCell{
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var dataLbl: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -26,13 +27,19 @@ class DataCell:UITableViewCell{
     
 }
 
-class ProfileVC: UIViewController,UITableViewDataSource, UITableViewDelegate, MFMailComposeViewControllerDelegate {
+class ProfileVC: UIViewController,UITableViewDataSource, UITableViewDelegate, MFMailComposeViewControllerDelegate, UIPageViewControllerDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var profilePicImgView: UIImageView!
     
+    var pageViewController: UIPageViewController!
+    
     var userData = [String]()
     let titleData = ["Name", "Email", "AUC ID", "Mobile"]
+    
+    let pageControllerIds = ["UserInfoId","PickUpId", "DropOffId"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -42,6 +49,10 @@ class ProfileVC: UIViewController,UITableViewDataSource, UITableViewDelegate, MF
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        setPageView()
     }
     
     
@@ -171,5 +182,77 @@ class ProfileVC: UIViewController,UITableViewDataSource, UITableViewDelegate, MF
     func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    // MARK: -Page View Controller Delegate
+    
+    func viewControllerAtIndex(index: Int) -> UIViewController{
+        
+        if ((self.pageControllerIds.count == 0) || (index >= self.pageControllerIds.count)) {
+            return ProfilePageVC()
+        }
+        
+        let vc: UIViewController = (self.storyboard?.instantiateViewControllerWithIdentifier("ArticlesView"))!
+        return vc
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?{
+        
+        let identifier = viewController.restorationIdentifier
+        var index = self.pageControllerIds.indexOf(identifier!)
+        
+        if (index == 0 || index == NSNotFound){
+            return nil
+        }
+        
+        (index!)--
+        
+        return self.viewControllerAtIndex(index!)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let identifier = viewController.restorationIdentifier
+        var index = self.pageControllerIds.indexOf(identifier!)
+        
+        if (index == NSNotFound){
+            return nil
+        }
+        
+        (index)!++
+        
+        if (index == self.pageControllerIds.count){
+            return nil
+        }
+        
+        return self.viewControllerAtIndex(index!)
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int{
+        return self.pageControllerIds.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int{
+        return 0
+    }
+    
+    func setPageView(){
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
+        
+        self.pageViewController.dataSource = self
+        
+        
+        var startVC = self.viewControllerAtIndex(0) as UIViewController
+        
+        var viewControllers = NSArray(object: startVC) as! NSMutableArray as UIViewController
+    
+        self.pageViewController.setViewControllers(viewControllers, direction: .Forward, animated: true, completion: nil)
+        
+        self.pageViewController.view.frame = CGRectMake(0, 60, self.view.frame.width, self.view.frame.size.height-70)
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
+    }
+
     
 }
